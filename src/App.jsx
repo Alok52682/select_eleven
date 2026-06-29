454cbd8bd5cc574c5c18ffd98ad44fde1465b579
Alok Rajbongshi <alokraj00061@gmail.com>
2026-06-29 15:43:10 +0600

    toggle added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import './App.css'
 import AvailablePlayers from './Components/AvailablePlayers/AvailablePlayers'
 import Navbar from './Components/Navbar/Navbar'
@@ -12,16 +12,26 @@ const fetchPlayers = async () => {
 
 
 function App() {
+  const [Available, setAvailable] = useState(true);
 
   const playersPromise = fetchPlayers()
 
   return (
     <>
       <Navbar />
-      <Suspense fallback={<span className="loading loading-spinner loading-md"></span>}>
-        <AvailablePlayers playersPromise={playersPromise} />
-      </Suspense>
-      <SelectedPlayers />
+      <div className='max-w-[1200px] mx-auto flex justify-between items-center'>
+        <h1 className='font-bold'>{Available ? 'Available Players' : 'Selected Players'}</h1>
+        <div>
+          <button onClick={() => setAvailable(true)} className={`py-3 px-4 border border-gray-400 rounded-l-xl md:w-30 border-r-0 ${Available && 'bg-amber-300'}`}>Available</button>
+          <button onClick={() => setAvailable(false)} className={`py-3 px-4 border border-gray-400 rounded-r-xl border-l-0 md:w-30 ${!Available && 'bg-amber-300'}`}>Selceted <span>{0}</span></button>
+        </div>
+      </div>
+      {Available ?
+        <Suspense fallback={<span className="loading loading-spinner loading-md"></span>}>
+          <AvailablePlayers playersPromise={playersPromise} />
+        </Suspense> :
+        <SelectedPlayers />
+      }
     </>
   )
 }
